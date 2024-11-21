8fe5a48c0de4bda054ae3069046991b94d011f42
jcardoso <joaoflaviofreitas@gmail.com>
2024-11-21 13:50:47 -0300

    4096 to 1024

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -92,10 +92,10 @@ void	create_list(t_list **list, int fd)
 
 char	*get_next_line(int fd)
 {
-	static t_list	*list[4096];
+	static t_list	*list[1024];
 	char			*next_line;
 
-	if (fd < 0 || fd > 4095 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
+	if (fd < 0 || fd > 1023 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
 		return (NULL);
 	create_list(list, fd);
 	if (!list[fd])
